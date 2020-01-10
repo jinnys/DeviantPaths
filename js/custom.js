fcbd523a1748b632cc0e1b7b28fed4cfef951611
Jinny Seesaw <JSeesaw@protonmail.com>
2020-01-10 14:14:51 +0800

    Page Changes

--- js/custom.js
+++ js/custom.js
@@ -54,6 +54,17 @@ $(window).scroll(function () {
 window.addEventListener('load', function() {
 	$('a.smooth').smoothScroll({speed: 800});
 });
+window.addEventListener('load', function() {
+if (!navigator.userAgent.match(/iPhone|iPad|iPod|Android|BlackBerry|IEMobile/i)) {
+	var skr = skrollr.init(
+		{
+		smoothScrolling: false
+		, forceHeight: false
+		, mobileDeceleration: 0.004
+		}
+		);
+	}
+});
 
 $('.single-iframe-popup').magnificPopup({
 	type: 'iframe',
--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 "use strict";
 
+ $(window).load(function () {
+	//------------------------------------------------------------------------
+	//						PRELOADER SCRIPT
+	//------------------------------------------------------------------------
+	$("#preloader").delay(400).fadeOut("slow");
+	$("#preloader .clock").fadeOut();
+});
+
 document.addEventListener('DOMContentLoaded', function() {
 
 
